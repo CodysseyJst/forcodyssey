5d5eeaa9f71e3e39b700662a40b135a4a4e97054
CodysseyJst <jst0504@gmail.com>
2025-07-15 16:19:16 +0900

    Add files via upload

---
+++ power_calculator.py
@@ -0,0 +1,41 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "Hello, DevOps!"
+
+if __name__ == '__main__':
+    def power_calculator(a, b):    
+        result = 1
+        i = 0
+        while i < b:
+            result = result * a
+            i += 1
+        return result
+
+    while True:
+        try:
+            number = float(input("Enter number:"))
+            global num
+            num = number
+            break
+        except ValueError:
+            print("Invalid number input.")
+            continue
+
+    while True:
+        try:
+            exponent = int(input("Enter number:"))
+            global exp
+            exp = exponent
+            break
+        except ValueError:
+            print("Invalid exponent input.")
+            continue
+    
+    result = power_calculator(num,exp)
+    print("Result:",result)
+
+   
